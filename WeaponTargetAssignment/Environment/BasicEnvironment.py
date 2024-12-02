132aed0fcd94863489654c6582b45d763fd14a97
jeffreychan177 <jeffreychan177@icloud.com>
2024-12-02 18:52:35 +1030

    change registration

--- WeaponTargetAssignment/Environment/BasicEnvironment.py
+++ WeaponTargetAssignment/Environment/BasicEnvironment.py
@@ -3,17 +3,9 @@ from gymnasium import spaces
 import numpy as np
 
 class BasicEnvironment(gym.Env):
-    def __init__(
-        self, 
-        num_weapons=3, 
-        num_targets=5, 
-        max_distance=15, 
-        front_line=1, 
-        reset_callback=None, 
-        reward_callback=None, 
-        observation_callback=None, 
-        render_mode=None
-    ):
+    metadata = {"render_modes": ["human"]}
+
+    def __init__(self, num_weapons=3, num_targets=5, max_distance=15, front_line=1, render_mode=None):
         super(BasicEnvironment, self).__init__()
 
         # Parameters
@@ -21,11 +13,6 @@ class BasicEnvironment(gym.Env):
         self.num_targets = num_targets
         self.max_distance = max_distance
         self.front_line = front_line
-
-        # Callbacks
-        self.reset_callback = reset_callback
-        self.reward_callback = reward_callback
-        self.observation_callback = observation_callback
 
         # Define shared observation space
         self.observation_space = spaces.Box(
@@ -46,9 +33,13 @@ class BasicEnvironment(gym.Env):
             for _ in range(self.num_weapons)
         ]
 
+        # Combine all action spaces into a single Tuple space
+        self.action_space = spaces.Tuple(self.action_spaces)
+
         self.shared_reward = True
         self.render_mode = render_mode
         self.reset()
+
 
 
     def reset(self, seed=None, options=None):
