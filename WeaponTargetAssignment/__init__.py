527c5bba865746b74fc4e547f91f134c5a0d9adf
jeffreychan177 <jeffreychan177@icloud.com>
2024-12-02 18:41:57 +1030

    change assignment

--- WeaponTargetAssignment/__init__.py
+++ WeaponTargetAssignment/__init__.py
@@ -1,17 +1,16 @@
 from WeaponTargetAssignment.Environment.Scenario import generate_scenario
 from gymnasium import register
 
+# Get the Scenario instance
 scenario = generate_scenario()
-world = scenario.create_world()
 
 register(
     id='Environment-v0',
     entry_point='WeaponTargetAssignment.Environment:BasicEnvironment',
     kwargs={
-        'world': world,
+        'world': scenario.world,  # Access the world directly from the Scenario
         'reset_callback': scenario.reset_world,
         'reward_callback': scenario.reward,
-        'observation_callback': scenario.observation
+        'observation_callback': scenario.observation,
     }
 )
-
