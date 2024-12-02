1f0d4c74d0c8476a9007ab316970407f918c3b6e
jeffreychan177 <jeffreychan177@icloud.com>
2024-12-02 18:48:17 +1030

    change registration

--- WeaponTargetAssignment/__init__.py
+++ WeaponTargetAssignment/__init__.py
@@ -1,16 +1,17 @@
 from WeaponTargetAssignment.Environment.Scenario import generate_scenario
 from gymnasium import register
 
-
+# Generate the scenario
 scenario = generate_scenario()
 
+# Register the environment
 register(
     id='Environment-v0',
-    entry_point='WeaponTargetAssignment.Environment:BasicEnvironment',
+    entry_point='WeaponTargetAssignment.Environment.BasicEnvironment',
     kwargs={
-        'world': scenario.world, 
-        'reset_callback': scenario.reset_world,
-        'reward_callback': scenario.reward,
-        'observation_callback': scenario.observation,
+        'num_weapons': 3, 
+        'num_targets': 5,
+        'max_distance': 15,
+        'front_line': 1,
     }
 )
