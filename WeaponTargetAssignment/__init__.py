2a35dbaca180d3d5c815688b90c2282edfbed58a
jeffreychan177 <jeffreychan177@icloud.com>
2024-12-18 17:05:40 +1030

    refactor 3

--- WeaponTargetAssignment/__init__.py
+++ WeaponTargetAssignment/__init__.py
@@ -1,24 +1,21 @@
 from itertools import product
 from gymnasium import register
-from WeaponTargetAssignment.Environment.Scenario import Scenario
+from Scenario import Scenario
 
 # Define parameter ranges
 partial_obs_options = [True, False]
-num_agents_options = [1, 3, 5]      
+num_agents_options = [1, 2, 5]      
 num_stages_options = [1,2,3]         
-num_targets_options = [5, 10] 
+num_targets_options = [5, 10]       
 
-# For each combination, create a scenario and register a unique environment ID
 for po, na, ns, nt in product(partial_obs_options, num_agents_options, num_stages_options, num_targets_options):
-    # Create a scenario with given parameters (assuming Scenario can handle them)
-    scenario = Scenario(n_agents=na, num_targets=nt, partial_obs=po)
+    scenario = Scenario(n_agents=na, num_targets=nt)
     world = scenario.make_world()
 
     env_id = f"WTA-Po{int(po)}-A{na}-Stg{ns}-T{nt}-v0"
-    
     register(
         id=env_id,
-        entry_point='WeaponTargetAssignment.Environment:MultiStageEnvironment',
+        entry_point='MultiStageEnvironment:MultiStageEnvironment',
         kwargs={
             'world': world,
             'reset_callback': scenario.reset_world,
