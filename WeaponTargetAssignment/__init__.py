ca450952a7c35fdb87468412fe7832e0bb347fd9
jeffreychan177 <jeffreychan177@icloud.com>
2024-12-18 16:34:42 +1030

    refactor

--- WeaponTargetAssignment/__init__.py
+++ WeaponTargetAssignment/__init__.py
@@ -1,49 +1,32 @@
-from WeaponTargetAssignment.Environment.Scenario import Scenario
+from itertools import product
 from gymnasium import register
+from WeaponTargetAssignment.Environment.Scenario import Scenario
 
-scenario = Scenario()
-world = scenario.make_world()
+# Define parameter ranges
+partial_obs_options = [True, False]
+num_agents_options = [1, 3, 5]      
+num_stages_options = [1,2,3]         
+num_targets_options = [5, 10] 
 
-register(
-    id='Environment-v0',
-    entry_point='WeaponTargetAssignment.Environment:SingleStageEnvironment',
-    kwargs={
-        'world': world,
-        'reset_callback': scenario.reset_world,
-        'reward_callback': scenario.reward,
-        'observation_callback': scenario.observation
-    }
-)
+# For each combination, create a scenario and register a unique environment ID
+for po, na, ns, nt in product(partial_obs_options, num_agents_options, num_stages_options, num_targets_options):
+    # Create a scenario with given parameters (assuming Scenario can handle them)
+    scenario = Scenario(n_agents=na, num_targets=nt, partial_obs=po)
+    world = scenario.make_world()
 
-register(
-    id='Environment-v1',
-    entry_point='WeaponTargetAssignment.Environment:TwoStageEnvironment',
-    kwargs={
-        'world': world,
-        'reset_callback': scenario.reset_world,
-        'reward_callback': scenario.reward,
-        'observation_callback': scenario.observation
-    }
-)
-
-register(
-    id='Environment-v2',
-    entry_point='WeaponTargetAssignment.Environment:MultiStageEnvironment',
-    kwargs={
-        'world': world,
-        'reset_callback': scenario.reset_world,
-        'reward_callback': scenario.reward,
-        'observation_callback': scenario.observation
-    }
-)
-
-register(
-    id='Environment-v3',
-    entry_point='WeaponTargetAssignment.Environment:FullyObservableSingleStageEnvironment',
-    kwargs={
-        'world': world,
-        'reset_callback': scenario.reset_world,
-        'reward_callback': scenario.reward,
-        'observation_callback': scenario.observation
-    }
-)
+    env_id = f"WTA-Po{int(po)}-A{na}-Stg{ns}-T{nt}-v0"
+    
+    register(
+        id=env_id,
+        entry_point='WeaponTargetAssignment.Environment:MultiStageEnvironment',
+        kwargs={
+            'world': world,
+            'reset_callback': scenario.reset_world,
+            'reward_callback': scenario.reward,
+            'observation_callback': scenario.observation,
+            'num_stages': ns,
+            'partial_obs': po,
+            'n_agents': na,
+            'num_targets': nt
+        }
+    )
