8c6f178104f34c65d38a3d5aa0e6d0e51c2ce5c9
jeffreychan177 <jeffreychan177@icloud.com>
2024-12-18 17:19:17 +1030

    buig fix

--- WeaponTargetAssignment/__init__.py
+++ WeaponTargetAssignment/__init__.py
@@ -1,12 +1,12 @@
 from itertools import product
 from gymnasium import register
-from Scenario import Scenario
+from WeaponTargetAssignment.Environment.Scenario import Scenario
 
 # Define parameter ranges
 partial_obs_options = [True, False]
 num_agents_options = [1, 2, 5]      
 num_stages_options = [1,2,3]         
-num_targets_options = [5, 10]       
+num_targets_options = [3, 5, 10]       
 
 for po, na, ns, nt in product(partial_obs_options, num_agents_options, num_stages_options, num_targets_options):
     scenario = Scenario(n_agents=na, num_targets=nt)
@@ -15,7 +15,7 @@ for po, na, ns, nt in product(partial_obs_options, num_agents_options, num_stage
     env_id = f"WTA-Po{int(po)}-A{na}-Stg{ns}-T{nt}-v0"
     register(
         id=env_id,
-        entry_point='MultiStageEnvironment:MultiStageEnvironment',
+        entry_point='WeaponTargetAssignment.Environment:MultiStageEnvironment',
         kwargs={
             'world': world,
             'reset_callback': scenario.reset_world,
